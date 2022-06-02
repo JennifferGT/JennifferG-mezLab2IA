6d1417f5197c348dde8500e5aadbbcdd8d70586b
JennifferGomez <jenniffer.jg59@gmail.com>
2022-06-02 10:50:59 -0500

    Corrige los comentarios de cada linea de codigo

--- bfs.py
+++ bfs.py
@@ -1,4 +1,6 @@
-"En el presente codigo permite obtener el recorrido y el orden de los nodos del grafo "
+'''En el presente codigo permite obtener el recorrido y el orden de los nodos del grafo,
+mediante los vertices asignados en cada nodo, recordando que solo puede ser visitado una vez cada nodo
+ indicando si el nodo es dirigido o no, cuando son dirigidos debe seguir su direcciín indicada'''
 
 from queue import Queue
 #Crea una función llamada gráfico.
@@ -7,35 +9,42 @@ class Grafico:
     def __init__(self, numero_de_nodos, dirigido=True):
         #Ingresa por medio de atributos el número de nodos.
         self.m_numero_de_nodos = numero_de_nodos
-        #Ingresa el número de nodos mediante el rango.
+        #Ingresa el número de nodos mediante el rango usando el atributo numero de nodos.
         self.m_nodos = range(self.m_numero_de_nodos)
-       
-        # Indica si es dirigido o no, mediante un verdadero o falso.
+        # Instancia el atributo dirigido.
         self.m_dirigido = dirigido
        
         #Se implementa una lista adyacente mediante un diccionario.
+        '''Para agregar un diccionario tenemos como parametro crear un objeto por lista agregando
+        un bucle de recorrido que empieza de nodo=0 hasta el rango asignado'''
         self.m_adj_lista = {nodo: set() for nodo in self.m_nodos}      
    
-    # Se agrega una función inicializando los nodos agregando un borde al gráfico.
+    # Se agrega un constructor instanciado declarando como atributo dos nodos y el peso.
+    '''Permite agregar los dos y el peso de los bordes para cada nodo, verificando si el nodo es dirigido 
+     o no, cuando el nodo es dirigido debe veridicar que el siguiente tenga el permiso o la dirección correcta 
+     para continuar al siguiente nodo, cuando no es dirigido puede ir por cualquier dirección'''
     def add_edge(self, nodo1, nodo2, weight=1):
         #Si el nodo1 es dirigido agrega un vertice del nodo1 al nodo2
         self.m_adj_lista[nodo1].add((nodo2, weight))
         # Cuando el nodo no está dirigido.
         if not self.m_dirigido:
             #Si el nodo2 no es dirigido agrega un vertice del nodo2 al nodo1
-            self.m_adj_lista[nodo2].add((nodo1, weight))
-   
+         self.m_adj_lista[nodo2].add((nodo1, weight))
+    ''' Para imprimir la lsita de los nodos y su peso de borde se debe agregar la instancia como atributo, 
+    agregar un bucle de recorrido que permita imprimir el posicion del nodo, con los respectivos datos 
+    almacenados en el diccionario de la lista adyacente imprimiendo en el orden del primer nodo'''
     # Imprime la representación gráfica del atributo.
     def print_adj_lista(self):
         #Realiza un sentencia identificando el número de llaves que están conectadas al nodo.
         for llave in self.m_adj_lista.keys():
-            #Imprime el número de nodos, con el número de llaves conectadas.
+            #Imprime en orden el número de nodos ingresados con los datos almacenados en el diccionaro
             print("nodo", llave, ": ", self.m_adj_lista[llave])
  
-    #Realiza una función que permite imprimir el número de recorridos que pasa por un vértice dado.
+    #En el constrructor se agrega los atributos de la instancia y el nodo inicial.
     def bfs_traversal(self, iniciar_nodo):
-        #Determina el conjunto de nodos visitados y realiza colas sencillas.
+        #el atributo visitado almacena en una lista los nodos recorridos sin ser repetidos
         visitado = set()
+        #Almacena los datos despues de ser visitados    2
         queue = Queue()
  
         # Mediante la lista de visitados y la cola se procede a añadir el nodo.
@@ -71,8 +80,7 @@ if __name__ == "__main__":
     valor6=int(input("Ingrese un número: "))
     valor7=int(input("Ingrese un número: "))
     valor8=int(input("Ingrese un número: "))
-    valor9=int(input("Ingrese un número: "))
-    # Agrega cinco gráficos con sus respectivos pesos de border.
+    # Agrega los atributos del gráficos con sus respectivos pesos de border.
     g.add_edge(valor1, valor2,valor5)
     g.add_edge(valor1,valor3,valor6)
     g.add_edge(valor2,valor5,valor1)
